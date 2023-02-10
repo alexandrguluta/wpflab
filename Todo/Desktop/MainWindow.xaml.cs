087635cff7a91b65705667b97ea661b7d65e11c1
Alexandr Guluta <guluta32@gmail.com>
2023-02-10 14:23:11 +0300

    lab1

---
+++ Todo/Desktop/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            //if (this.EmailisValid(EmailTextBox.Text))
+           // {
+                // То что проходит
+           // }
+           // else
+           // {
+            //    // То что не проходит (ошибка)
+           // }
+        }
+        void OnTextBoxTextChanged(object sender, TextChangedEventArgs e) //FIXME
+        {
+            if (EmailTextBox.Text == "Введите email")
+            {
+                // Create an ImageBrush.BBBBGB
+                ImageBrush textImageBrush = new ImageBrush();
+                textImageBrush.ImageSource =
+                    new BitmapImage(
+                        new Uri(@"TextBoxBackground.gif", UriKind.Relative)
+                    );
+                textImageBrush.AlignmentX = AlignmentX.Left;
+                textImageBrush.Stretch = Stretch.None;
+                // Use the brush to paint the button's background.
+                EmailTextBox.Background = textImageBrush;
+            }
+            else
+            {
+
+                EmailTextBox.Background = null;
+            }
+        }
+        bool EmailisValid(string email)
+        {
+            string pattern = "[.\\-_a-z0-9]+@([a-z0-9][\\-a-z0-9]+\\.)+[a-z]{2,6}";
+            Match isMatch = Regex.Match(email, pattern, RegexOptions.IgnoreCase);
+            return isMatch.Success;
+        }
+    }
+}
---
+++ Todo/Desktop/Validator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desktop
+{
+    public class Validator
+    {
+        public static string validityCheck(string pass)
+        { if (pass.Length >= 7)
+            { return null; }
+            else {
+                return "Error"; 
+                    }
+    }
+}
